edce9e0623e768b4d8fad5f6bb168cca09d20cd7
OviedoRobotics <54600937+OviedoRobotics@users.noreply.github.com>
2020-01-20 13:40:12 -0500

    Update auto for pulling foundation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/WayPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/WayPoint.java
@@ -1,16 +1,14 @@
 package org.firstinspires.ftc.teamcode.HelperClasses;
 
 public class WayPoint {
-    public WayPoint(double X, double Y, double angle, double speed, boolean passThrough){
+    public WayPoint(double X, double Y, double angle, double speed){
         this.x = X;
         this.y = Y;
         this.angle = angle;
         this.speed = speed;
-        this.passThrough = passThrough;
     }
     public double x;
     public double y;
     public double angle;
     public double speed;
-    public boolean passThrough;
 }
